1a8bc6e5acedc2fb08c779da39c0123c712b1aa6
Cyrus Blake <cyrusblake89@gmail.com>
2025-03-21 15:47:08 -0400

    more changes

--- phone-pass-app/vite.config.js
+++ phone-pass-app/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: '/', // Base public path
+  build: {
+    outDir: 'dist', // Output directory for the build
+  },
+  plugins: [react()], // React plugin for Vite
+});
