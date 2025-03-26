4d5d7345a47a1b868c8a07d21cd350a70c4fdbf6
Cyrus Blake <cyrusblake89@gmail.com>
2025-03-26 19:37:37 -0400

    more changes

--- phone-pass-app/src/components/Message.jsx
+++
@@ -1,8 +0,0 @@
-// const sendMessage = async (senderId, receiverId, message) => {
-//     await setDoc(doc(db, "messages", `${senderId}_${receiverId}`), {
-//       senderId,
-//       receiverId,
-//       message,
-//       timestamp: new Date(),
-//     });
-//   };
