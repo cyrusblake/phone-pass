ccbf88a8e3907df37ebee7622312991acbc52e62
Cyrus Blake <cyrusblake89@gmail.com>
2025-04-05 23:37:47 -0400

    more changes

--- phone-pass-app/src/pages/FriendsList.jsx
+++ phone-pass-app/src/pages/FriendsList.jsx
@@ -6,6 +6,7 @@ import BottomNav from "../components/BottomNav";
 import { getUserFriends } from "../api/firestore";
 import '../styles/components/friendslist.css';
 import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
+import ChatIcon from '@mui/icons-material/Chat';
 
 const FriendsList = () => {
     const { user } = useAuth();
@@ -52,11 +53,19 @@ const FriendsList = () => {
                     <div className="friends-grid">
                         {friends.map((friend, index) => (
                             <div key={index} className="friend-card">
-                           
+                                
                                 <Link to={`/viewPage/${friend.userId}`}>
+                                    
                                     <div className="friend-card-content">
+                                        
                                         <AccountCircleRoundedIcon className="home-pp" sx= { {fontSize: 40}}/>
-                                        <h3 className="f-usernames">{friend.username}</h3>
+                                        
+                                        <div className="friend-details">
+                                            <h3 className="f-usernames">{friend.username}</h3>
+                                            <ChatIcon className="friend-icon" sx={{ fontSize: 20 }} />
+                                        </div>
+
+
                                     </div>
                                     
                                 </Link>
