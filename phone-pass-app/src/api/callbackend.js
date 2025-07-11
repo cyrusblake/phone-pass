c34f878c29d45650cb0e2792b788bbb7a0b3314c
Cyrus Blake <cyrusblake89@gmail.com>
2025-07-11 00:03:44 -0400

    more changes

--- phone-pass-app/src/api/callbackend.js
+++ phone-pass-app/src/api/callbackend.js
@@ -1,6 +1,6 @@
 async function fetchFromBackend() {
   try {
-    const response = await fetch('https://fastapi-example-ly1n.onrender.com', {
+    const response = await fetch('https://fastapi-example-ly1n.onrender.com/api/data', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ test: "value" })
@@ -16,7 +16,7 @@ function TestConnection() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    fetch('https://fastapi-example-ly1n.onrender.com')
+    fetch('https://fastapi-example-ly1n.onrender.com/api/data')
       .then(res => res.json())
       .then(setData);
   }, []);
